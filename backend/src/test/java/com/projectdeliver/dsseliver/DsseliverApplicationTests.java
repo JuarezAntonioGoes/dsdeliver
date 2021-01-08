4350a2d5f18cee6c84a42076e63efb556aee44c1
JuarezGoesJunior <juarezantoniog12@gmail.com>
2021-01-08 12:07:51 -0300

    Project Created

---
+++ backend/src/test/java/com/projectdeliver/dsseliver/DsseliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectdeliver.dsseliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsseliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
